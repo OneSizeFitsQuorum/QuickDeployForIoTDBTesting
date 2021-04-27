ad021b321d3fef06f3d8245a4f30f97de85a2fa8
LebronAl <TXYPotato@gmail.com>
2021-04-27 09:50:04 +0800

    set seednodes

--- local/cluster.sh
+++ local/cluster.sh
@@ -6,6 +6,16 @@ rm -rf ${snapshotPath}1 ${snapshotPath}2
 cp -rf $snapshotPath ${snapshotPath}1
 cp -rf $snapshotPath ${snapshotPath}2
 
+##统一 seeds_node 
+sed -i -e 's/^seed_nodes=127.0.0.1:9003$/seed_nodes=127.0.0.1:9003,127.0.0.1:9005,127.0.0.1:9007/g' ${snapshotPath}/conf/iotdb-cluster.properties
+sed -i -e 's/^seed_nodes=127.0.0.1:9003$/seed_nodes=127.0.0.1:9003,127.0.0.1:9005,127.0.0.1:9007/g' ${snapshotPath}1/conf/iotdb-cluster.properties
+sed -i -e 's/^seed_nodes=127.0.0.1:9003$/seed_nodes=127.0.0.1:9003,127.0.0.1:9005,127.0.0.1:9007/g' ${snapshotPath}2/conf/iotdb-cluster.properties
+
+##打开 open_server_rpc_port 
+sed -i -e 's/^open_server_rpc_port=false$/open_server_rpc_port=true/g' ${snapshotPath}/conf/iotdb-cluster.properties
+sed -i -e 's/^open_server_rpc_port=false$/open_server_rpc_port=true/g' ${snapshotPath}1/conf/iotdb-cluster.properties
+sed -i -e 's/^open_server_rpc_port=false$/open_server_rpc_port=true/g' ${snapshotPath}2/conf/iotdb-cluster.properties
+
 #处理 jmx port 冲突
 sed -i -e 's/JMX_PORT="31999"/JMX_PORT="32000"/g' ${snapshotPath}1/conf/iotdb-env.sh
 sed -i -e 's/JMX_PORT=31999/JMX_PORT=32000/g' ${snapshotPath}1/conf/iotdb-env.bat
@@ -20,5 +30,5 @@ sed -i -e 's/internal_meta_port=9003/internal_meta_port=9007/g' ${snapshotPath}2
 sed -i -e 's/internal_data_port=40010/internal_data_port=40014/g' ${snapshotPath}2/conf/iotdb-cluster.properties
 
 #处理 standalone port 冲突
-sed -i -e 's/rpc_port=6667/rpc_port=6668/g' ${snapshotPath}1/conf/iotdb-engine.properties
-sed -i -e 's/rpc_port=6667/rpc_port=6669/g' ${snapshotPath}2/conf/iotdb-engine.properties
+sed -i -e 's/rpc_port=6667/rpc_port=6669/g' ${snapshotPath}1/conf/iotdb-engine.properties
+sed -i -e 's/rpc_port=6667/rpc_port=6671/g' ${snapshotPath}2/conf/iotdb-engine.properties
