16b6a47bd23bdd623dfd1ab07f105c60f1343b62
LebronAl <TXYPotato@gmail.com>
2021-10-29 10:56:32 +0800

    update

--- remote/config.sh
+++ remote/config.sh
@@ -7,7 +7,7 @@ user="root"
 #服务器新建的文件夹名称
 serverDir="txy"
 #打包的文件名
-snapshotName="iotdb-cluster-0.13.0-SNAPSHOT"
+snapshotName="iotdb-cluster-0.12.3-SNAPSHOT"
 #本地 cluster 模块 maven 打包路径
 snapshotPath="/Users/txy/Study/iotdb/cluster/target/${snapshotName}"
 #测试集群的执行路径
@@ -15,8 +15,8 @@ serverPath="~/${serverDir}/${snapshotName}"
 #从测试集群拉取得到的执行日志本地路径
 localLogDir="result"
 #更改 iotdb-cluster.properties 中的属性，用|隔开，格式为 key=value，即 key 为参数名称，value 为值的方式替代对应文件中的参数
-clusterConfig="seed_nodes=${seeds}"
+clusterConfig="seed_nodes=${seeds}|default_replica_num=1"
 #更改 iotdb-engine.properties 中的属性，用|隔开，格式为 key=value，即 key 为参数名称，value 为值的方式替代对应文件中的参数
-engineConfig="enable_wal=false"
+engineConfig=""
 #是否后台启动集群
 execute=true
